064b015bbacf900f5904064e42a278950357ce32
clementokot <clementokot@gmail.com>
2026-03-24 21:51:02 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -8,28 +8,21 @@ int main(){
     scanf("%f", &units);
     printf("enter the cost per unit:");
     scanf("%f", &cost_per_unit);
-
     if (units <= 100){
         bill_amount = 0.5 * units * cost_per_unit;
         printf("the bill amount is: %f\n", bill_amount);
-
     }
-
     else if (units > 100 && units <= 200){
         bill_amount = 0.75 * units * cost_per_unit;
         printf("the bill amount is: %f\n", bill_amount);
     }
-
     else if (units > 200 && units <= 300){
         bill_amount = 1.20 * units * cost_per_unit;
         printf("the bill amount is: %f\n", bill_amount);
-
     }
-
     else if (units > 300){
         bill_amount = 1.50 * units *cost_per_unit;
         printf("the bill amount is: %f\n", bill_amount);
     }
-    return 0;
-        
+    return 0;  
     }
